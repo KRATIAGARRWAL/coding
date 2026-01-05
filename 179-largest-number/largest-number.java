0e4cba8ba8c9870044283ffaa46afd6b5fb889b1
KRATIAGARRWAL <130988757+KRATIAGARRWAL@users.noreply.github.com>
2026-01-05 21:46:28 +0530

    Time: 7 ms (27.93%) | Memory: 46 MB (11.04%) - LeetSync

--- 179-largest-number/largest-number.java
+++ 179-largest-number/largest-number.java
@@ -2,17 +2,19 @@ class Solution {
     int compare(String a,String b){
         int n1=a.length();
         int n2=b.length();
+        String temp1= a+b;
+        String temp2=b+a;
+        a=temp1;
+        b=temp2;
         int i=0;
-        while(i<n1 && i<n2){
+        while(i<temp1.length()){
             if(a.charAt(i)>b.charAt(i))
             return -1;
             else if(a.charAt(i)<b.charAt(i)) return 1;
             i++;
         }
-        if(n1==n2) return 0;
+        return 0;
 
-        if(n1>n2) return compare(a.substring(n2),b);
-        return compare(a,b.substring(n1));
     }
     public String largestNumber(int[] nums) {
         ArrayList<String> temp=new ArrayList<>();
