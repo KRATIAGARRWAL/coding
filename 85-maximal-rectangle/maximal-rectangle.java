7bbc8c65410ab3e7df0465b7204f306967801dca
KRATIAGARRWAL <130988757+KRATIAGARRWAL@users.noreply.github.com>
2026-01-11 22:46:54 +0530

    Time: 4 ms (93.88%) | Memory: 50.3 MB (13.47%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.java
+++ 85-maximal-rectangle/maximal-rectangle.java
@@ -1,37 +1,34 @@
 class Solution {
     public int maximalRectangle(char[][] matrix) {
-        int m=matrix.length;
-        int n=matrix[0].length;
+        int m = matrix.length;
+        int n = matrix[0].length;
 
-        int arr[]=new int[n];
-        int ans=0;
+        int[] arr = new int[n];
+        int ans = 0;
 
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(matrix[i][j]=='0') arr[j]=0;
+        for (int i = 0; i < m; i++) {
+
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == '0') arr[j] = 0;
                 else arr[j]++;
             }
-            Stack<Integer> stk=new Stack<>();
 
-            for(int j=0;j<=n;j++){
-                int h= (j==n) ? -1: arr[j];
-                
-                while(stk.size()>0 && h < arr[stk.peek()]){
-                    int l=arr[stk.pop()];
-                    int b=(stk.size()==0) ?j :j- stk.peek()-1;
-                    ans=Math.max(ans, l*b);
+            int[] stk = new int[n + 1];
+            int top = -1;
 
+            for (int j = 0; j <= n; j++) {
+                int h = (j == n) ? -1 : arr[j];
+
+                while (top != -1 && h < arr[stk[top]]) {
+                    int height = arr[stk[top--]];
+                    int width = (top == -1) ? j : j - stk[top] - 1;
+                    ans = Math.max(ans, height * width);
                 }
 
-                stk.push(j);
+                stk[++top] = j;
             }
-           
         }
 
-
         return ans;
-
     }
 }
-
-
