7d8edad9c7843d24485efd9185f2c64c44434e5f
KRATIAGARRWAL <130988757+KRATIAGARRWAL@users.noreply.github.com>
2026-01-13 19:24:30 +0530

    Time: 6 ms (26.49%) | Memory: 81.4 MB (5.00%) - LeetSync

--- 97-interleaving-string/interleaving-string.java
+++ 97-interleaving-string/interleaving-string.java
@@ -1,5 +1,5 @@
 class Solution {
-    Boolean dp[][];
+    Boolean dp[][][];
     public boolean find(int i, int j,int k, String a, String b, String c){
 
         if(i==a.length() && j==b.length() && k==c.length())
@@ -11,15 +11,15 @@ class Solution {
             return a.substring(i).equals(c.substring(k));
         }
 
-        if(dp[i][j]!=null) return dp[i][j];
+        if(dp[i][j][k]!=null) return dp[i][j][k];
         
 
-        if(a.charAt(i)==c.charAt(k) && find(i+1,j,k+1,a,b,c))return dp[i][j]=true;
+        if(a.charAt(i)==c.charAt(k) && find(i+1,j,k+1,a,b,c))return dp[i][j][k]=true;
 
-        return dp[i][j]=b.charAt(j)==c.charAt(k) && find(i,j+1,k+1,a,b,c);
+        return dp[i][j][k]=b.charAt(j)==c.charAt(k) && find(i,j+1,k+1,a,b,c);
     }
     public boolean isInterleave(String a, String b, String c) {
-        dp= new Boolean [a.length()][b.length()];
+        dp= new Boolean [a.length()][b.length()][a.length()+b.length()];
         if(a.length()+b.length()!= c.length()) return false;
         return find(0,0,0,a,b,c);
     }
