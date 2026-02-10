23474c4f86ccb6bd1d4913ab600a68ffe2467ec3
KRATIAGARRWAL <130988757+KRATIAGARRWAL@users.noreply.github.com>
2026-02-10 22:53:36 +0530

    Time: 20 ms (93.58%) | Memory: 46.4 MB (74.75%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.java
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.java
@@ -1,38 +1,47 @@
 class Solution {
-    public int find(int i, int j, int y, int [][]grid, int dp[][][]){
-        if(dp[i][j][y]!=-1) return dp[i][j][y];
-        int m=grid.length;
-        int n=grid[0].length;
-        int ans=grid[i][j];
-        if(j!=y){
-            ans+=grid[i][y];
-        }
-        if(i==m-1)return dp[i][j][y]= ans;
-        int temp=0;
-        for(int a=-1;a<2;a++){
-            int j1=j+a;
-            if(j1>=0 && j1<n)
-            for(int b=-1;b<2;b++){
-                int y1=y+b;
-                if(y1>=0 && y1<n){
-                    temp=Math.max(temp,find(i+1,j1,y1,grid,dp));
-                }
-            }
-        }
-        return dp[i][j][y]=ans+temp;
-    }
     public int cherryPickup(int[][] grid) {
-        int m=grid.length;
-        int n=grid[0].length;
+        int m = grid.length;
+        int n = grid[0].length;
 
-        int dp[][][]=new int[m][n][n];
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                for(int k=0;k<n;k++){
-                    dp[i][j][k]=-1;
+        int[][][] dp = new int[m][n][n];
+
+        // ---------- Base case: last row ----------
+        for (int j = 0; j < n; j++) {
+            for (int y = 0; y < n; y++) {
+                if (j == y)
+                    dp[m-1][j][y] = grid[m-1][j];
+                else
+                    dp[m-1][j][y] = grid[m-1][j] + grid[m-1][y];
+            }
+        }
+
+        // ---------- Fill from bottom to top ----------
+        for (int i = m - 2; i >= 0; i--) {
+            for (int j = 0; j < n; j++) {
+                for (int y = 0; y < n; y++) {
+
+                    int ans = grid[i][j];
+                    if (j != y) ans += grid[i][y];
+
+                    int best = 0;
+
+                    for (int a = -1; a <= 1; a++) {
+                        int j1 = j + a;
+                        if (j1 < 0 || j1 >= n) continue;
+
+                        for (int b = -1; b <= 1; b++) {
+                            int y1 = y + b;
+                            if (y1 < 0 || y1 >= n) continue;
+
+                            best = Math.max(best, dp[i+1][j1][y1]);
+                        }
+                    }
+
+                    dp[i][j][y] = ans + best;
                 }
             }
         }
-        return find(0,0,grid[0].length-1,grid,dp);
+
+        return dp[0][0][n-1];
     }
 }
